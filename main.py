f974692e6fcde25fec5b3516c3d99ecff6f497c2
OnBlockHocTaco <aviralavnimishra25@gmail.com>
2023-05-29 16:23:56 -0400

    Correctness for when first value is smaller than later values and maintain usage of subtraction and division

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def add(x, y):
     return x + y
 
 def subtract(x, y):
-    if x - y < 0:
+    #NYT Lets you subtract numbers such that x - y = 0; however, creating a 0 value can never be an optimal step
+    #In addition and subtracting, using a 0 is equivalent to just starting with that number
+    #In multiplication and division, using a 0 is either not possible or is equivalent to starting with that number
+    #NYT does not let you subtract so as to create a negative number
+    if x - y <= 0:
         return None
     return x - y
 
@@ -16,34 +20,26 @@ def divide(x, y):
     else:
         return x // y
 
-operations = [add, subtract, multiply, divide]
-operations_in_text = [" + ", " - ", " * ", " / "]
+operation_to_text = {add:" + ", subtract:" - ", multiply:" * ", divide : " / "}
 
 def digit_solver(target, nums):
     def helper(remain):
-        #if len(remain) == 1:
-        #    return (False, "")
-        i = 0
-        while i < len(remain) - 1:
-            j = i + 1
-            while j < len(remain):
-                counter = 0
-                while counter < 4:
-                    operation = operations[counter]
-                    new_number = operation(remain[i], remain[j])
+        for i in range(len(remain) - 1):
+            for j in range(i + 1, len(remain)):
+                max_val = max(remain[i], remain[j])
+                min_val = min(remain[i], remain[j])
+                for operation in operation_to_text.keys():
+                    new_number = operation(max_val, min_val)
                     if new_number == None:
                         break
-                    operation_string = str(remain[i]) + operations_in_text[counter] + str(remain[j]) + " = " + str(new_number) + ", "
+                    operation_string = str(max_val) + operation_to_text[operation] + str(min_val) + " = " + str(
+                        new_number) + ", "
                     if new_number == target:
                         return (True, operation_string)
-                    new_array = [new_number] + remain[:i] + remain[i+1:j] + remain[j+1:]
-
+                    new_array = [new_number] + remain[:i] + remain[i + 1:j] + remain[j + 1:]
                     res = helper(new_array)
                     if res[0]:
                         return (True, operation_string + res[1])
-                    counter += 1
-                j += 1
-            i += 1
         return (False, "")
 
     found_solution = helper(nums)
@@ -51,10 +47,9 @@ def digit_solver(target, nums):
         return "No Solution Found"
     return found_solution[1]
 
-
 def main():
     print(digit_solver(341, [5, 7, 8, 9, 15, 20]))
-    print(digit_solver(43, [3, 9, 13, 19, 20, 23]))
+    print(digit_solver(6, [3, 9]))
 
 
 if __name__ == "__main__":
